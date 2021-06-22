e71cd5be4151944d025aa87c5ed0f437b4d6654a
slevasseur2021 <slevasseur2021@102SE13-GMH3P02.ad.campus-eni.fr>
2021-06-22 16:49:30 +0200

    ADD AccueilCOnnecte, MAJ ServletConnexion

--- src/fr/eni/dal/jdbc/UtilisateurDAOImplt.java
+++ src/fr/eni/dal/jdbc/UtilisateurDAOImplt.java
@@ -13,8 +13,8 @@ import fr.eni.dal.UtilisateurDAO;
 
 public class UtilisateurDAOImplt implements UtilisateurDAO {
 	
-	private static final String FIND_BY_EMAIL = "SELECT * FROM UTILISATEUR WHERE email=? AND motDePasse =?";
-	private static final String FIND_BY_PSEUDO = "SELECT * FROM UTILISATEUR WHERE pseudo=? AND motDePasse=?";
+	private static final String FIND_BY_EMAIL = "SELECT * FROM UTILISATEUR WHERE email=? AND mot_de_passe =?";
+	private static final String FIND_BY_PSEUDO = "SELECT * FROM UTILISATEUR WHERE pseudo=? AND mot_de_passe=?";
 	
 	@Override
 	public boolean verifier(Utilisateur utilisateur) {
@@ -36,7 +36,7 @@ public class UtilisateurDAOImplt implements UtilisateurDAO {
 					utilisateur.setRue(rs.getString("rue"));
 					utilisateur.setCodePostal(rs.getString("codePostal"));
 					utilisateur.setVille(rs.getString("ville"));
-					utilisateur.setMotDePasse(rs.getString("motDePasse"));
+					utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
 					utilisateur.setCredit(rs.getInt("credit"));
 					utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
 					existe = true;
@@ -60,7 +60,7 @@ public class UtilisateurDAOImplt implements UtilisateurDAO {
 					utilisateur.setRue(rs.getString("rue"));
 					utilisateur.setCodePostal(rs.getString("codePostal"));
 					utilisateur.setVille(rs.getString("ville"));
-					utilisateur.setMotDePasse(rs.getString("motDePasse"));
+					utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
 					utilisateur.setCredit(rs.getInt("credit"));
 					utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
 					existe = true;
